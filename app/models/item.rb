7a47c8bf5b6351d973b27e0fbc5079ef52338c04
koguma-san <swymmrn.o.28@gmail.com>
2024-05-24 05:48:24 +0900

    バリデーションの設定

--- app/models/item.rb
+++ app/models/item.rb
@@ -11,9 +11,10 @@ class Item < ApplicationRecord
 
   validates :item_name, presence: true
   validates :item_details, presence: true
-  validates :category_id, numericality: { other_than: 1, message: "can't be blank" }
-  validates :condition_id, numericality: { other_than: 1, message: "can't be blank" }
-  validates :delivery_charge_id, numericality: { other_than: 1, message: "can't be blank" }
-  validates :prefecture_id, numericality: { other_than: 1, message: "can't be blank" }
-  validates :delivery_day_id, numericality: { other_than: 1, message: "can't be blank" }
+  validates :category_id, numericality: { other_than: 1, message: "can't be blank" }, presence: true
+  validates :condition_id, numericality: { other_than: 1, message: "can't be blank" }, presence: true
+  validates :delivery_charge_id, numericality: { other_than: 1, message: "can't be blank" }, presence: true
+  validates :prefecture_id, numericality: { other_than: 1, message: "can't be blank" }, presence: true
+  validates :delivery_day_id, numericality: { other_than: 1, message: "can't be blank" }, presence: true
+  validates :price, presence: true
 end
