1333d6aa5cbd13e93aee691980f2f5dea0c852fd
koguma-san <swymmrn.o.28@gmail.com>
2024-06-16 14:43:47 +0900

    手数料と利益の表示設定内容修正

--- app/javascript/item_price.js
+++ app/javascript/item_price.js
@@ -1,16 +1,23 @@
 function post (){
   const priceInput = document.getElementById("item-price");
-  priceInput.addEventListener("input", () => {
-    const inputValue = priceInput.value;
+  if (priceInput) {
+    if (!priceInput.dataset.listenerAdded) {
+      priceInput.addEventListener("input", () => {
+        const inputValue = priceInput.value;
 
-    const addTaxDom = document.getElementById("add-tax-price");
-    const taxAmount = Math.floor(inputValue * 0.1);
-    addTaxDom.innerHTML = taxAmount;
+        const addTaxDom = document.getElementById("add-tax-price");
+        const taxAmount = Math.floor(inputValue * 0.1);
+        addTaxDom.innerHTML = taxAmount;
 
-    const addProfitDom = document.getElementById("profit");
-    const profitAmount = Math.floor(inputValue - taxAmount);
-    addProfitDom.innerHTML = profitAmount;
-  });
+        const addProfitDom = document.getElementById("profit");
+        const profitAmount = Math.floor(inputValue - taxAmount);
+        addProfitDom.innerHTML = profitAmount;
+      });
+      priceInput.dataset.listenerAdded = "true"; 
+    }
+  }
 };
 
 window.addEventListener('turbo:load', post);
+window.addEventListener('turbo:render', post);
+window.addEventListener('DOMContentLoaded', post);
